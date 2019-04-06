77e72d846549d7562fc3d2cfe9f90c2f6b5fad8c
Test Machine <testmachine@Tests-MacBook-Pro.local>
2019-04-06 18:09:25 -0400

    Refactor Model

---
+++ app/src/main/java/com/wattpad/mystory/model/entity/ArticleCollection.kt
@@ -0,0 +1,17 @@
+package com.wattpad.mystory.model.entity
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class ArticleCollection {
+
+    @SerializedName("status")
+    @Expose
+    var status: String? = null
+    @SerializedName("totalResults")
+    @Expose
+    var totalResults: Int? = null
+    @SerializedName("articles")
+    @Expose
+    var articles: List<Article>? = null
+}
