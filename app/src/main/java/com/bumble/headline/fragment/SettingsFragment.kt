4165bd5e4606a0ee1584b085b15d89c99a304b40
Test Machine <testmachine@Tests-MacBook-Pro.local>
2019-04-09 07:26:10 -0400

    Settings Page Country Selection

--- app/src/main/java/com/bumble/headline/fragment/SettingsFragment.kt
+++ app/src/main/java/com/bumble/headline/fragment/SettingsFragment.kt
@@ -1,13 +1,17 @@
 package com.bumble.headline.fragment
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.bumble.headline.R
 import com.bumble.headline.viewmodel.SettingsViewModel
+import kotlinx.android.synthetic.main.settings_fragment.*
 
 
 class SettingsFragment : Fragment() {
@@ -29,6 +33,25 @@ class SettingsFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         // TODO: Use the ViewModel
-    }
 
+        val categories = ArrayList<String>()
+        categories.addAll(viewModel.getCountryList(context))
+
+        val dataAdapter = ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, categories)
+        countrySpinner.adapter = dataAdapter
+
+        countrySpinner.onItemSelectedListener = object : OnItemSelectedListener {
+
+            override fun onItemSelected(parentView: AdapterView<*>, selectedItemView: View, position: Int, id: Long) {
+                countryText.text = categories[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
+
+        updateCountryButton.setOnClickListener {
+           // countryText.text = viewModel.countrySpinnerSelected
+        }
+    }
 }
--- app/src/main/java/com/bumble/headline/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/bumble/headline/viewmodel/SettingsViewModel.kt
@@ -1,7 +1,21 @@
 package com.bumble.headline.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.bumble.headline.R
 
 class SettingsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    var countrySpinnerSelected:String = ""
+
+    fun getCountryList(context: Context?): ArrayList<String> {
+        val countries = context!!.resources.getStringArray(R.array.country_arrays)
+        var result:ArrayList<String> = ArrayList()
+
+        countries.forEach {
+            result.add(it)
+        }
+        return result
+    }
+
 }
